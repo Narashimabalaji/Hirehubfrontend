8d51e575fd6504fff48935d579aa8cca585aa5d6
Narashimabalaji <116134721+Narashimabalaji@users.noreply.github.com>
2025-06-14 18:57:37 +0700

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -45,7 +45,7 @@ const Login = () => {
     const userType = activeTab === 0 ? 'seeker' : 'hirer';
   
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://hirehubbackend-5.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
